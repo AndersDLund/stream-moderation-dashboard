918e6f7ba4c393bb325d2ef1a19ec602e7ad2331
Anders Lund <anderslund@Anderss-MacBook-Pro.local>
2021-06-11 13:00:55 -0600

    added basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.scss';
 
+// components
+import { FlaggedMessages } from './components/FlaggedMessages/FlaggedMessages';
+
+//assets
+import { ReactComponent as StreamLogo } from './assets/stream_logo.svg';
+
 const App = () => {
   return (
     <main>
-      this is where it all happens
+      <header>
+        <h1>Application: Moderation Bois</h1>
+      </header>
+      {/* Need to make components for all the sections here -- these are just placeholder for layout now */}
+      <section className="container"></section>
+      <FlaggedMessages />
+      <section className="container"></section>
+      <section className="container"></section>
+      <section className="logo-container">
+        <StreamLogo className="stream-logo" />
+      </section>
     </main>
   )
 }
---
+++ src/components/FlaggedMessages/FlaggedMessages.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './FlaggedMessages.scss';
+
+export const FlaggedMessages = () => {
+    return (
+        <section className="container"></section>
+    )
+}
