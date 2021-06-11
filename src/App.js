9a400b25da2fb372aa1af8d5336959b2019e206e
Ryan Kavanaugh <ryankavanaugh@Ryans-MacBook-Pro.local>
2021-06-11 14:20:51 -0500

    Ryan's first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 
 // components
 import { FlaggedMessages } from './components/FlaggedMessages/FlaggedMessages';
+import { ChannelList } from './components/ChannelList/ChannelList';
 
 //assets
 import { ReactComponent as StreamLogo } from './assets/stream_logo.svg';
@@ -10,10 +11,10 @@ const App = () => {
   return (
     <main>
       <header>
-        <h1>Application: Moderation Bois</h1>
+        <h1>Application: Moderation Bois!</h1>
       </header>
       {/* Need to make components for all the sections here -- these are just placeholder for layout now */}
-      <section className="container"></section>
+      <ChannelList />
       <FlaggedMessages />
       <section className="container"></section>
       <section className="container"></section>
