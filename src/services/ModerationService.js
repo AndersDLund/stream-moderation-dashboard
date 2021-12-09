191fe3df43e4d666842aa6ffacbdbb4a7cea7ab8
Ryan Kavanaugh <ryankavanaugh@ryans-mbp.attlocal.net>
2021-12-09 15:53:18 -0600

    new client updates Anders/Ryan

--- src/services/ModerationService.js
+++ src/services/ModerationService.js
@@ -13,5 +13,10 @@ export const ModerationService = {
         console.log(text, 'TEXT');
         const url = `${process.env.REACT_APP_BASE_URL}/moderation/sentiment`;
         return axios.post(url, { text });
-    }
+    },
+    BanUser: (userIds) => {
+        const url = `${process.env.REACT_APP_BASE_URL}/moderation/banUser`;
+        console.log(userIds)
+        // return axios.post(url, { userIds });
+    },
 }
